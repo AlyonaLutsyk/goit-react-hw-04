e905a1d990d7a8ca91898de618cc46a9d72e3942
AlyonaLutsyk <148708226+AlyonaLutsyk@users.noreply.github.com>
2024-09-14 17:56:10 +0300

    fixed

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,12 @@
 import css from './App.module.css';
 import { useState, useEffect } from 'react';
-import SearchBar from "../SearchBar/SearchBar";
-import ImageGallery from '../ImageGallery/ImageGallery';
-import Loader from '../Loader/Loader';
+import SearchBar from "../components/SearchBar/SearchBar";
+import ImageGallery from '../components/ImageGallery/ImageGallery';
+import Loader from '../components/Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
-import ImageModal from '../ImageModal/ImageModal';
-import { fetchImages } from '../../unsplashApi';
+import LoadMoreBtn from '../components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from '../components/ImageModal/ImageModal';
+import { fetchImages } from '../unsplashApi';
 import { Toaster } from 'react-hot-toast';
 
 

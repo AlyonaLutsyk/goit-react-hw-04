738db2bc5e22dc3630e1552d3e4d8aabb8fbb991
AlyonaLutsyk <148708226+AlyonaLutsyk@users.noreply.github.com>
2024-09-14 17:40:51 +0300

    fix-hw

--- src/Components/ErrorMessage/ErrorMessage.jsx
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -1,5 +1,4 @@
-import css from './ErrorMessage.module.css';
 
 export default function ErrorMessage({ message }) {
-  return <div className={css.error}>{message}</div>;
+  return <div>{message}</div>;
 }
